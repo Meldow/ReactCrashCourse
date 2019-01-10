4299bcec6d753183db5dd3f759c18bfbf7c9c4ba
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-10 18:42:58 +0000

    feat(02): Add badge

--- 02/counter-app/src/components/counter.jsx
+++ 02/counter-app/src/components/counter.jsx
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
+
 //https://youtu.be/Ke90Tje7VS0?t=2486
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
     };
 
     render() {
+        let classes = this.getBadgeClasses();
+
         return (
             <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className={classes}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {

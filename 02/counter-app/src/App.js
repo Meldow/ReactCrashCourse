13887ff8f7edc0a69ebba0561a031302288f3e22
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-14 18:47:22 +0000

    feat(02): Lyft the state and add navbar

--- 02/counter-app/src/App.js
+++ 02/counter-app/src/App.js
@@ -3,16 +3,52 @@ import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 
 class App extends Component {
+    state = {
+        counters: [
+            {id: 1, value: 4, tags: []},
+            {id: 2, value: 2, tags: ["tag1"]},
+            {id: 3, value: 0, tags: []},
+            {id: 4, value: 0, tags: ["tag2", "tag3"]},
+        ]
+    }
+
     render() {
         return (
             <React.Fragment>
-                <Navbar/>
+                <Navbar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
                 <main className="container">
-                    <Counters/>
+                    <Counters
+                        counters={this.state.counters}
+                        onReset={this.handleReset}
+                        onIncrement={this.handleIncrement}
+                        onDelete={this.handleDelete}
+                    />
                 </main>
             </React.Fragment>
         );
     }
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        // to update, we clone the input we want to update
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    }
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters})
+    };
 }
 
 export default App;
--- 02/counter-app/src/components/counters.jsx
+++ 02/counter-app/src/components/counters.jsx
@@ -2,53 +2,22 @@ import React, {Component} from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 4, tags: []},
-            {id: 2, value: 2, tags: ["tag1"]},
-            {id: 3, value: 0, tags: []},
-            {id: 4, value: 0, tags: ["tag2", "tag3"]},
-        ]
-    }
-
     render() {
         return (
             <React.Fragment>
                 <button
-                    onClick={this.handleReset}
+                    onClick={this.props.onReset}
                     className="btn btn-primary btn-sm m-2">Reset
                 </button>
-                {this.state.counters.map(counter =>
+                {this.props.counters.map(counter =>
                     <Counter key={counter.id}
-                             onDelete={this.handleDelete}
-                             onIncrement={this.handleIncrement}
+                             onDelete={this.props.onDelete}
+                             onIncrement={this.props.onIncrement}
                              counter={counter}
                     />)}
             </React.Fragment>
         );
     }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        // to update, we clone the input we want to update
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({counters});
-    }
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters});
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({counters})
-    };
 }
 
 export default Counters;

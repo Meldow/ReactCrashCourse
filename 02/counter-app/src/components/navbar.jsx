c1dc765f0fc5b713c9c8f5873b009bce3714d912
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-15 09:17:28 +0000

    refactor(02): Add property destructor simpify props

--- 02/counter-app/src/components/navbar.jsx
+++ 02/counter-app/src/components/navbar.jsx
@@ -4,13 +4,13 @@ import React, {Component} from 'react';
 // since we have no calculations or state
 // simple stateless component
 
-const Navbar = (props) => {
+const Navbar = ({totalCounters}) => {
     return (
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
                 Navbar{" "}
                 <span className="badge badge-pill badge-secondary">
-                    {props.totalCounters}
+                    {totalCounters}
                 </span>
             </a>
         </nav>

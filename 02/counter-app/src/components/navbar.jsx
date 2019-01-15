ee599d8be514778e719cf9937df5c3014d87fccd
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-15 09:12:35 +0000

    feat(02): Navbar is now a stateless component

--- 02/counter-app/src/components/navbar.jsx
+++ 02/counter-app/src/components/navbar.jsx
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar{" "}
-                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        );
-    }
-}
+// Stateless Functional Component
+// since we have no calculations or state
+// simple stateless component
+
+const Navbar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar{" "}
+                <span className="badge badge-pill badge-secondary">
+                    {props.totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
+};
 
 export default Navbar;

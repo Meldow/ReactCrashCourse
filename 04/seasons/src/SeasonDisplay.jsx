e72bcb15eabec4218eb8b18951968e8e72825821
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-15 16:25:37 +0000

    feat(04): Wrap up seasons project

--- 04/seasons/src/SeasonDisplay.jsx
+++ 04/seasons/src/SeasonDisplay.jsx
@@ -1,8 +1,39 @@
+import './SeasonDisplay.css'
 import React, {Component} from 'react';
 
-
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
 }
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0
+            ? 'summer'
+            : 'winter';
+    }
+    return lat < 0
+        ? 'summer'
+        : 'winter';
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon massive icon-left`}/>
+            <h1>{text}</h1>
+            <i className={`${iconName} icon massive icon-right`}/>
+        </div>
+    )
+};
 
 export default SeasonDisplay;
--- 04/seasons/src/index.js
+++ 04/seasons/src/index.js
@@ -1,41 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
 
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                console.log(position);
-                this.setState({
-                    lat: position.coords.latitude
-                });
-            },
-            (error) => {
-                console.log(error);
-                this.setState({
-                    errorMessage: error.message
-                });
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            error => this.setState({errorMessage: error.message})
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
-        return <div>Loading...</div>
+        return <Spinner message="Please accept location request"/>
+    }
+
+    render() {
+        return (
+            <div className="border red">{this.renderContent()}</div>
+        )
     }
 }
 
